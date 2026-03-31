6f19c4c353e60d36dde188fc30c98032844f82fd
eubrunoo07 <devbruninn@outlook.com>
2026-03-31 17:35:06 -0300

    first configurations on project

--- ledgerflow-infra/database/initdb/create-multiple-dbs.sh
+++ ledgerflow-infra/database/initdb/create-multiple-dbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for db in catalog inventory sales losses replenishment_orders; do
+for db in account transaction; do
     echo "Creating database: $db"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
         CREATE DATABASE "$db";
