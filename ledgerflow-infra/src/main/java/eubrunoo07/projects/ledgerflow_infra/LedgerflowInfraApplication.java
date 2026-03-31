02956a4622bf893d5e7a29be08a5cfd4f66d7e4e
eubrunoo07 <devbruninn@outlook.com>
2026-03-31 17:31:06 -0300

    first commit

---
+++ ledgerflow-infra/src/main/java/eubrunoo07/projects/ledgerflow_infra/LedgerflowInfraApplication.java
@@ -0,0 +1,13 @@
+package eubrunoo07.projects.ledgerflow_infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerflowInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerflowInfraApplication.class, args);
+	}
+
+}
---
+++ ledgerflow-infra/src/test/java/eubrunoo07/projects/ledgerflow_infra/LedgerflowInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package eubrunoo07.projects.ledgerflow_infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerflowInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
