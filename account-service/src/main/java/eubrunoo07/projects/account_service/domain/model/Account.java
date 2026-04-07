7e4f2adc8b763bb679308852a775e3a6d7cd0dcb
eubrunoo07 <devbruninn@outlook.com>
2026-04-07 16:06:44 -0300

    feat: create account endpoint

--- account-service/src/main/java/eubrunoo07/projects/account_service/domain/model/Account.java
+++ account-service/src/main/java/eubrunoo07/projects/account_service/domain/model/Account.java
@@ -27,7 +27,6 @@ public class Account {
 
     public Account(String ownerName) {
         this.ownerName = ownerName;
-        this.id = UUID.randomUUID();
         this.balance = BigDecimal.ZERO;
         this.createdAt = LocalDateTime.now();
     }
--- account-service/src/main/java/eubrunoo07/projects/account_service/service/impl/AccountServiceImpl.java
+++ account-service/src/main/java/eubrunoo07/projects/account_service/service/impl/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package eubrunoo07.projects.account_service.service.impl;
 
+import eubrunoo07.projects.account_service.domain.model.Account;
 import eubrunoo07.projects.account_service.repository.AccountRepository;
 import eubrunoo07.projects.account_service.service.AccountService;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,11 @@ public class AccountServiceImpl implements AccountService {
 
     public AccountServiceImpl(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Account createAccount(String ownerName) {
+        Account account = new Account(ownerName);
+        return accountRepository.save(account);
     }
 }
