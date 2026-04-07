f0a93456922c35ae433b257f2cd279e8f40597b4
eubrunoo07 <devbruninn@outlook.com>
2026-04-07 15:48:58 -0300

    chore: setting all configurations

---
+++ account-service/src/main/java/eubrunoo07/projects/account_service/service/impl/AccountServiceImpl.java
@@ -0,0 +1,15 @@
+package eubrunoo07.projects.account_service.service.impl;
+
+import eubrunoo07.projects.account_service.repository.AccountRepository;
+import eubrunoo07.projects.account_service.service.AccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+}
